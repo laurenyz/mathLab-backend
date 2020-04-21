ac932f8dd5cd5991866fb29b4f3356ee7b95d7ea
laurenyz <laurenkyu@gmail.com>
2020-04-21 13:03:42 -0400

    added some controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all 
+        
+        render json: users, include: [:posts, :replies]
+    end
+
+    def show
+        user = User.find(params[:id])
+
+        render json: user, include: [:posts, :replies]
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :posts
     has_many :replies, class_name: "Reply", foreign_key: "replier_id"
     has_many :upvotes, through: :replies
+
+    def total_upvotes
+        self.upvotes.count
+    end
 end
