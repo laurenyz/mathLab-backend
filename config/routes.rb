bce21becfb5f6003f744263ef913163510848fd3
laurenyz <laurenkyu@gmail.com>
2020-05-01 17:47:47 -0400

    basic setup for active storage complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
+  post '/users/:id/upload_image', to: 'users#uploadImage'
   mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
