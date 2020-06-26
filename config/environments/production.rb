26cef01596d473b973a031b4e48442a8abcc549d
laurenyz <laurenkyu@gmail.com>
2020-06-26 00:14:36 -0400

    updating action cable allowed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://laurenyz-mathlab.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = ["http://localhost:3000"]
+  config.action_cable.allowed_request_origins = ["https://laurenyzmathlab.netlify.app/"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
