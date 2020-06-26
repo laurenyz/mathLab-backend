1376ea66cd18e486a0e1fa78b181d6368b377080
laurenyz <laurenkyu@gmail.com>
2020-06-26 00:09:22 -0400

    action cable allowed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://laurenyz-mathlab.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ["http://localhost:3000/"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
